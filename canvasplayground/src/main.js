73ce62a94e3b3c3c2dc339e3090f72f55f0c151e
elskeptico <128253345+elskeptico@users.noreply.github.com>
2025-12-31 15:13:50 -0800

    its a square

--- canvasplayground/src/main.js
+++ canvasplayground/src/main.js
@@ -1,3 +1,8 @@
 const canvas = document.querySelector("#board canvas");
 const ctx = canvas.getContext("2d");
 console.log(ctx.canvas);
+
+ctx.strokeStyle = "black";
+ctx.fillStyle = "red";
+ctx.fillRect(300, 200, 50 , 50)
+ctx.strokeRect(300, 200, 50, 50)
