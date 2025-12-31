9a27808cc9226c581c9e734c860caa9c8036834c
elskeptico <128253345+elskeptico@users.noreply.github.com>
2025-12-31 15:37:11 -0800

    I need to remember to save

--- canvasplayground/src/circlysquare.js
+++ canvasplayground/src/circlysquare.js
@@ -1,8 +1,16 @@
+//setup
 const canvas = document.querySelector("#board canvas");
 const ctx = canvas.getContext("2d");
 console.log(ctx.canvas);
 
+//square
 ctx.strokeStyle = "black";
 ctx.fillStyle = "red";
 ctx.fillRect(300, 200, 50 , 50)
 ctx.strokeRect(300, 200, 50, 50)
+
+//circle
+ctx.beginPath();
+ctx.arc(325, 170, 25, 0, Math.PI * 2, false);
+ctx.fill();
+ctx.stroke();
