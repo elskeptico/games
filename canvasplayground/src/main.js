9d604ecd55f0e886dcc59739859923f0aadd40c7
elskeptico <128253345+elskeptico@users.noreply.github.com>
2025-12-31 15:46:03 -0800

    experimented with text

--- canvasplayground/src/main.js
+++ canvasplayground/src/main.js
@@ -1,25 +1,12 @@
-//setup
 const canvas = document.querySelector("#board canvas");
 const ctx = canvas.getContext("2d");
-const { width: w, height: h } = canvas;
 
-//background black
-ctx.fillStyle = "black";
-ctx.fillRect(0,0, w, h);
+ctx.font = "20pt courier"
 
-//cosmic gray
-ctx.fillStyle = "#555"
+const center = w / 2
+ctx.textAlign = "center"
 
-//draw stars
-let x, y, radius;
-
-for (let i = 0; i < 550; i++) {
-    x = Math.random() * w;
-    y = Math.random() * h;
-    radius = Math.random() * 3
-
-    //draw star
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2, false);
-    ctx.fill();
+for (let i = 0; i < 11; i++) {
+    ctx.fillText("If you're in the game", center, i * 40);
 }
+ctx.strokeText("strokes the word", center, h - 30);
