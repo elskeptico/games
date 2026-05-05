dc904df2eb9e6462994ee0ffc656f8f4ef06f10b
elskeptico <128253345+elskeptico@users.noreply.github.com>
2026-05-05 23:12:23 +0000

    alr guys

--- playground.js
+++ playground.js
@@ -5,3 +5,9 @@ const world = new World();
 
 // Start simulation
 const testbed = Testbed.start(world);
+
+let platform = world.createBody({
+  type: "static",
+  position: {x: 0, y: -10},
+  angle: Math.PI * 0.1
+});
