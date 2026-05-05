c98f7a72e3a0aa0cdd3045b044af7803702e6bd2
elskeptico <128253345+elskeptico@users.noreply.github.com>
2026-05-05 18:57:49 +0000

    ttly did it guys

--- playground.js
+++ playground.js
@@ -1,6 +1,12 @@
-const { World, Testbed } = planck;
 // Create a world
-const world = new World();
+let world = new World({
+  gravity: {x: 0, y: -10},
+});
 
 // Start simulation
 const testbed = Testbed.start(world);
+let platform = world.createBody({
+  type: "static",
+  position: {x: 0, y: -10},
+  angle: Math.PI * 0.1
+});
